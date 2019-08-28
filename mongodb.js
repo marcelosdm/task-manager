ed90a337073ceee18464030bec313434e3221f44
Marcelo Melo <marcelosdm@gmail.com>
2019-08-28 00:01:22 -0300

    Add config files and create new documents in db

---
+++ mongodb.js
@@ -0,0 +1,41 @@
+const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient
+
+const databaseName = 'task-app'
+const connectionURL = `mongodb://mmelo_dev:solhi1986@ds145563.mlab.com:45563/${databaseName}`
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.warn('Unable to connect to database!')
+    }
+    console.log('Connected to database!')
+
+    const db = client.db(databaseName)
+    // db.collection('users').insertOne({
+    //   name: 'Marcelo',
+    //   age: 32
+    // })
+    db.collection('tasks').insertMany(
+      [
+        {
+          description: 'Work at LuizaLabs',
+          completed: false
+        },
+        {
+          description: 'Be the best I can in Java',
+          completed: false
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          console.warn(`Couldn't save documents`)
+        }
+        console.log(result.ops)
+      }
+    )
+  }
+)
