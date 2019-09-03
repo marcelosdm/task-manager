6137af1fb70ee5bba64b4650effc01f2349a7b1e
Marcelo Melo <marcelosdm@gmail.com>
2019-09-02 23:19:14 -0300

    Add GET routes

--- src/index.js
+++ src/index.js
@@ -18,6 +18,25 @@ app.post('/users', (req, res) => {
     .catch(e => res.status(400).send(e))
 })
 
+app.get('/users', (req, res) => {
+  User.find({})
+    .then(users => res.send(users))
+    .catch(e => res.status(500).send())
+})
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id
+
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send()
+      }
+      res.send(user)
+    })
+    .catch(e => res.status(500).send())
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
 
@@ -25,6 +44,25 @@ app.post('/tasks', (req, res) => {
     .save()
     .then(() => res.status(201).send(task))
     .catch(e => res.status(400).send(e))
+})
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then(tasks => res.send(tasks))
+    .catch(e => console.log(e))
+})
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id
+
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        res.status(404).send()
+      }
+      res.send(task)
+    })
+    .catch(e => res.status(500).send())
 })
 
 app.listen(port, () => console.log(`Server up on port: ${port}`))
