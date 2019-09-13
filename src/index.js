eabe23629fd484b9e741fe6dd4cd48c1db83701e
Marcelo Melo <marcelosdm@gmail.com>
2019-09-13 06:54:01 -0300

    Add bcrypt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
+const bcrypt = require('bcryptjs')
 
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -10,5 +11,18 @@ const port = process.env.PORT || '3000'
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+const myFunction = async () => {
+  const pass = '123456'
+  const hashedPass = await bcrypt.hash(pass, 8)
+
+  console.log(pass)
+  console.log(hashedPass)
+
+  const isMatch = await bcrypt.compare('123456', hashedPass)
+  console.log(isMatch)
+}
+
+myFunction()
 
 app.listen(port, () => console.log(`Server up on port: ${port}`))
